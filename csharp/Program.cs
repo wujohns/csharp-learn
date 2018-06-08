a217cf6441b7a5f4079069f91061845bbbd4fd1a
wujons <wujohns@163.com>
2018-06-08 17:43:11 +0800

    nuget的坑填完，后续继续填坑

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using Newtonsoft.Json;
 
-namespace csharp
-{
+namespace csharp {
+    public class Account {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DOB { get; set; }
+    }
+
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main (string[] args) {
+            Account account = new Account {
+                Name = "KK",
+                Email = "kk@blood.com",
+                DOB = new DateTime(1980, 2, 20, 0, 0, 0, DateTimeKind.Utc)
+            };
+
+            string jsonStr = JsonConvert.SerializeObject(account, Formatting.Indented);
+            Console.WriteLine(jsonStr);
         }
     }
 }
--- docs/unity_base.md
+++ docs/unity_base.md
@@ -1,6 +1,12 @@
 # unity 的基础部分
 这里记录 unity 中脚本开发的过程基础，作为之后的工程范式。
 
+// TODO 补充下述内容
+
 ## unity 工程结构
+
+## unity 包管理
+
+## untiy asset store 相关
 
 ## git 相关注意事项
